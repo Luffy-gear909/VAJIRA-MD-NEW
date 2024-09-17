7f89b1331b52c3c53e34a720a2cb61ea142c0202
Luffy-gear909 <hamzashahid1152901@gmail.com>
2024-09-18 04:05:43 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=vYtSzYbC#fTGxArjfnMJHFp0ZCKosVRebLfsP-o3WpxOvxBNDldM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=bV9XSLjA#HAchb2m20bbFzOjBdmyVHaxaYM9uyadVjsOQKnhtWYA' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '+212649406954' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
